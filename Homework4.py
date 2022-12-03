e436646c15932377fe5f2e75c1ff3bf585f6a42b
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-03 22:03:09 +0200

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -9,41 +9,34 @@ from helper import format_records_dict,format_records_lst
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 @app.route('/order-price')
 @use_kwargs(
     {
     "country": fields.Str(
-        missing=None,
+        missing='BillingCountry',
         validate=validate.Length(min=1,max=20)
         )
     },
     location='query'
 )
 def get_customers(country):
-    query = 'SELECT * FROM (SELECT * FROM invoices JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId) '
+    query = r'SELECT BillingCountry,count(*)*(Quantity*UnitPrice) FROM invoices JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId '
+    query += 'WHERE BillingCountry="'+ country+'" GROUP BY BillingCountry'
     records = execute_query(query)
-    dict_quantity = dict()
-
-    for i in records:
-        if i[6] not in dict_quantity:
-            dict_quantity[i[6]]= i[-1]*i[-2]
-        else:
-            dict_quantity[i[6]] += i[-1]*i[-2]
-    for i, y in dict_quantity.items():
-        if country != None and country not in dict_quantity.keys():
-            return 'Country not found'
-        elif i == country:
-            return str(i)+ ' ' + str(round(y,2))
-        dict_quantity.update({i:round(y,2)})
-    return format_records_dict(dict_quantity)
+    if len(records)==0:
+        return 'Not Found'
+    return format_records_lst(records)
 
 
 @app.route('/tracks-info')
 @use_kwargs(
     {
     "track_id":fields.Int(
-        missing=None,
+        missing='TrackId',
         validate=[validate.Range(min=1,max=5000)]
     )
     },
@@ -54,22 +47,18 @@ def get_all_info_about_tracks(track_id):
     query+=' JOIN albums ON tracks.AlbumId = albums.AlbumId'
     query+=' JOIN artists ON albums.ArtistId = artists.ArtistId'
     query+=' JOIN genres ON tracks.GenreId = genres.GenreId'
+    query+=' WHERE TrackId='+str(track_id)
     records = execute_query(query)
-    if track_id == None:
-        return format_records_lst(records)
-    for item in records:
-        if item[0] == track_id:
-            return str(item)
-    return 'Not found'
+    if len(records)==0:
+        return 'Not Found'
+    return format_records_lst(records)
 
 @app.route('/tracks-len')
 def tracks_len():
-    query = 'SELECT * FROM tracks'
+    query = 'SELECT sum(Milliseconds) FROM tracks'
     records = execute_query(query)
-    tracks_len_millis = 0
-    for item in records:
-        tracks_len_millis += item[6]
-    return str(round(tracks_len_millis/3600000,2)) + ' Hours'
+    return str(round(records[0][0]/3600000,2)) +' Hours'
+
 
 
 app.run(port=5001,debug=True)

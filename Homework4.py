2c314afe0947aeeefd18cbdb2cf4eabc971965ee
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-05 10:26:54 +0200

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -16,19 +16,22 @@ def hello():
 @app.route('/order-price')
 @use_kwargs(
     {
-    "country": fields.Str(
-        missing='BillingCountry',
+    "city": fields.Str(
+        missing=None,
         validate=validate.Length(min=1,max=20)
         )
     },
     location='query'
 )
-def get_customers(country):
-    query = r'SELECT BillingCountry,count(*)*(Quantity*UnitPrice) FROM invoices JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId '
-    query += 'WHERE BillingCountry="'+ country+'" GROUP BY BillingCountry'
+def get_customers(city):
+    query = 'SELECT BillingCity,count(*)*(Quantity*UnitPrice) FROM invoices JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId '
+    if city != None:
+        query += ' WHERE BillingCity="' + city + '" GROUP BY BillingCity'
+    else:
+        query += ' GROUP BY BillingCity'
     records = execute_query(query)
     if len(records)==0:
-        return 'Not Found'
+        return 'City is not found'
     return format_records_lst(records)
 
 
@@ -36,21 +39,25 @@ def get_customers(country):
 @use_kwargs(
     {
     "track_id":fields.Int(
-        missing='TrackId',
+        missing=None,
         validate=[validate.Range(min=1,max=5000)]
     )
     },
     location='query'
 )
 def get_all_info_about_tracks(track_id):
-    query = 'SELECT tracks.TrackId,tracks.Name,title,artists.Name,genres.Name,Milliseconds,Bytes,UnitPrice,media_types.Name FROM tracks JOIN media_types ON  tracks.MediaTypeId = media_types.MediaTypeId'
+    query = 'SELECT DISTINCT tracks.Name,albums.Title,artists.Name as Artist,genres.Name as Genre,Milliseconds as TimeMillis,Bytes as MemoryUsed,UnitPrice as Price,media_types.Name as MediaType,playlists.Name as Playlist FROM tracks '
+    query+=' JOIN media_types ON  tracks.MediaTypeId = media_types.MediaTypeId'
     query+=' JOIN albums ON tracks.AlbumId = albums.AlbumId'
     query+=' JOIN artists ON albums.ArtistId = artists.ArtistId'
     query+=' JOIN genres ON tracks.GenreId = genres.GenreId'
-    query+=' WHERE TrackId='+str(track_id)
+    query+=' JOIN playlist_track ON tracks.TrackId = playlist_track.TrackId'
+    query+=' JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId'
+    if track_id != None:
+        query+=' WHERE tracks.TrackId='+str(track_id)
     records = execute_query(query)
     if len(records)==0:
-        return 'Not Found'
+        return 'No such TrackID'
     return format_records_lst(records)
 
 @app.route('/tracks-len')

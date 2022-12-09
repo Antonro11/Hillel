c9fa01d0c0cd7ac51345041f7c2bcbae269372d8
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-09 14:50:58 +0200

    Create point_in_circle.py

---
+++ point_in_circle.py
@@ -0,0 +1,22 @@
+class Circle:
+    def __init__(self,x,y,radius):
+        self.x=x
+        self.y=y
+        self.radius=radius
+    def __contains__(self,point):
+        if ((point.x-self.x)**2+(point.y-self.y)**2)**0.5 <=self.radius:
+            return True
+        else:
+            return False
+        
+
+class Point:        
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+
+circle = Circle(2,2,4)
+point = Point(1,1)
+print(point in circle)
+print(Point(-3,-8) in Circle(2,2,4))

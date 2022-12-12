756be7c900de1649fb83744ee4f3420f91292dc0
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-12 11:23:52 +0200

    Update Float_range.py

--- Float_range.py
+++ Float_range.py
@@ -4,10 +4,7 @@ class frange:
         self.start = start
         self.finish = finish
         self.step = step
-        self.iterator = self.start - self.step
-
-        
-    def __iter__(self):
+        self.count = self.start - self.step
         if len(self.args) == 1:
             self.finish = self.args[0]
         elif len(self.args) == 2:
@@ -17,30 +14,35 @@ class frange:
             self.start = self.args[0]
             self.finish = self.args[1]
             self.step = self.args[2]
-            self.iterator = self.start - self.step
+            self.count = self.start - self.step
         else:
             return 'Error'
-        self.iterator =self.start - self.step
+
+        
+    def __iter__(self):        
+        self.count =self.start - self.step
         return self
     
     def __next__(self):
-        if self.finish > self.start:
-            if self.iterator + self.step < self.finish and self.step > 0:
-                self.iterator += self.step
-                return self.iterator
+            if self.count + self.step < self.finish and self.step > 0:
+                self.count += self.step
+                return self.count
+            elif self.count + self.step > self.finish and self.step < 0:
+                self.count += self.step
+                return self.count
             else:
                 raise StopIteration
-        else:
-            if self.iterator + self.step > self.finish and self.step < 0:
-                self.iterator += self.step
-                return self.iterator
-            else:
-                raise StopIteration
+               
         
 
 
   
+        
+        
+        
+
 print(list(frange(5, -5,-0.5)))
 print(list(frange(10)))
 print(list(frange(2,5,0.5)))
 print(list(frange(100,0)))
+

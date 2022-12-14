23f4c22790a3302a12a06e02a8204ca51a10df17
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-14 12:44:04 +0200

    Update lsp.py

--- lsp.py
+++ lsp.py
@@ -1,55 +1,48 @@
 """
-Liskov subtitution
+Liskov Supstitution
 
-"""
-class Student:
-    def __init__(self):
-        self.student_marks=dict()
-    
-    def add_student_marks(self,name,*marks):
-        marks_lst=[]
-        for i in marks:
-            marks_lst.append(i)
-            if type(i) != int or i<1 or i>100:
-                marks_lst.pop()
-        self.student_marks[name] = marks_lst
-    
-    def show_student_marks(self):
-        return self.student_marks
-
-
-class Group(Student):
-    def __init__(self):
-        super().__init__()
-    
-        
-    def show_student_rating(self,name):
-        for i in self.student_marks:
-            if name == i:
-                average_mark = sum(self.student_marks[name])//len(self.student_marks[name])
-                if 90<average_mark<=100:
-                    return name+' '+str(average_mark)+': Exellent' 
-                elif 80<average_mark<=90:
-                    return name+' '+str(average_mark)+': Very good' 
-                elif 70<average_mark<=80:
-                    return name+' '+str(average_mark)+': Good' 
-                elif 60<average_mark<=70:
-                    return name+' '+str(average_mark)+': Normal' 
-                elif 50<average_mark<=60:
-                    return name+' '+str(average_mark)+': Need to improve'
-                else:
-                    return name+' '+str(average_mark)+': Bad'
-        return 'Student is not found'
-
-"""
-   In the parent's class "Student" we need to have type(str) like a first argument,
-   and integers in range(1,100) in the all others, so we need a check. If the argument is not valid, we removing it.
+We have same data in Students and Teachers: (name,city,phone) but we have data which is not same: (group),(salary).
+So we created one class for (name,city,phone) and two more classas for (group) and (salary)
+  
 
 """
 
+import random
+from faker import Faker
+    
+class PersonInfo:
+    def __init__(self,name,city,phone):
+        self.name = name
+        self.phone = phone
+        self.city = city
+class Info_With_Group(PersonInfo):
+    def __init__(self,name,city,phone,group):
+        super().__init__(name,city,phone)
+        self.group = group
 
-a = Group()
-a.add_student_marks('Vasya',20,30,199,50)
-a.add_student_marks('Petya',20,45,78,92,14,'some text')
-a.add_student_marks('Vera',25,86,3,89,68,25,99)
-print(a.show_student_rating('Petya'))
+class Info_with_subject(PersonInfo):
+    def __init__(self,name,city,phone,subject):
+        super().__init__(name,city,phone)
+        self.subject = subject
+
+
+    
+    
+class Student(Info_With_Group):
+    def __init__(self, name, city, phone, group):
+        super().__init__(name, city, phone, group)
+    
+    def show_student(self):
+        return {self.name:(self.city,self.phone,self.group)} 
+
+class Teacher(Info_with_subject):
+    def __init__(self, name, city, phone, subject):
+        super().__init__(name, city, phone, subject)
+    
+    def show_teacher(self):
+        return {self.name:(self.city,self.phone,self.subject)} 
+    
+a = Student('Anton','Dnipro','+380999999','B')
+b = Teacher('Vera','Kiev','+380777777','mathematic')
+print(a.show_student())
+print(b.show_teacher())

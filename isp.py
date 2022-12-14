08e82762e90a1a0d642a7d74c19e6ab631a728b8
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-14 12:45:05 +0200

    Update isp.py

--- isp.py
+++ isp.py
@@ -1,41 +1,41 @@
 """
 Interface separeted
 
+We created class Group and Teachers, and we can calculate average or maximumm value of marks or salaries.
+So to create separate class for this calculation is better than put it in every class.
+
 """
 
 
-class Teacher:
+
+class Group_info:
     def __init__(self):
-        self.teachers = dict()
+        self.group = {
+            'Anton':88,
+            'Sergiy':75,
+            'Masha':95
+        }
 
-    def add_teacher_info(self,name,city,birthdate,salary):
-        self.teachers[name] = [city,birthdate,salary]
-
-class Group(Teacher):
+class Teachers_info:
     def __init__(self):
-        super().__init__()
-    
-    def show_teachers(self):
-        teachers =[i for i in self.teachers]
-        return teachers
-    def show_cities(self):
-        cities = [{i:y[0]} for i,y in self.teachers.items()]
-        return cities
-    def show_birthdates(self):
-        birthdates = [{i:y[1]} for i,y in self.teachers.items()]
-        return birthdates
-    def show_saralies(self):
-        salaries = [{i:y[2]} for i,y in self.teachers.items()]
-        return salaries
+        self.teachers ={
+            'Anatoliy':1700,
+            'Vitaliy':1800,
+            'Pavel':2100
+            }
 
-"""
-    We made many small functions opposite to one big 
+class Avarege_max:
+    def avarage(values):
+        return str(round(sum(values)/len(values),2))
+    def maximumm(values):
+        return max(values)
 
-"""
-    
-a = Group()
-a.add_teacher_info('Vera','Dnipro','20.04.95','1600')
-a.add_teacher_info('Petya','Kiev','19.5.91','1800')
-print(a.show_birthdates())
-print(a.show_cities())
-print(a.show_saralies())
+
+a = Group_info()
+b = Teachers_info()
+
+c = Avarege_max
+
+print(c.avarage(a.group.values()))
+print(c.maximumm(b.teachers.values()))
+

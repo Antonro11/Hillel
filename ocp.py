58ed082498a1d87cb659b190cd48c433e925d3cf
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-14 12:43:19 +0200

    Update ocp.py

--- ocp.py
+++ ocp.py
@@ -1,113 +1,61 @@
 """"
 Open - close 
 
+We created data in one class, and than we need to sort it. But we don't sort the Parent's class, we made additional
+class for sorting and there we create new sorted data 
+
 """
 
 import random
 from faker import Faker
 
 
-class Info:
+class  Group:
     def __init__(self):
-            self.lst_groups = ['A','B','C','D']
-            self.lst_subjects = ['mathematic','culture','chemistry','geography']
-            
+        self.group_info = []
+        self.groups = ['A','B','C','D']
 
-class Teacher(Info):
-    def __init__(self):
-        super().__init__()
-        self.teachers = list()
-    
-    def add_teachers(self,name,name_subject):
-        if name_subject in self.lst_subjects:
-            self.teachers.append([name_subject+':',name])
-        else:
-            print('Subject name is not found')
-    
-    def show_teachers(self):
-        teachers_str = ''
-        for lst in self.teachers:
-            for item in lst:
-                teachers_str+= item +' '
-            teachers_str+='\n'
-        return teachers_str[:-1]
+    def add_student(self,name_of_group,name_of_student,birthdate,city,phone):
+        self.group_info.append([name_of_group,name_of_student,birthdate,city,phone])
 
-        
-class Students(Info):
+    def remove_student(self,student_name):
+        for i in self.group_info:
+            if student_name in i:
+                self.group_info.remove(i)
+class Student(Group):
     def __init__(self):
         super().__init__()
-        self.students = list()
-    
-    def add_students(self,name,name_group,mark):
-        if name_group in self.lst_groups:
-            self.students.append(['Group '+name_group+':',name+',','average mark:',mark])
-        else:
-            print('Group is not found')
-    
-    def show_students(self):
-        students_str = ''
-        for lst in self.students:
-            for item in lst:
-                students_str += str(item) + ' '
-            students_str+='\n'
         
-        return students_str[:-1]
+    def show_student_info(self,student_name):
+        for i in self.group_info:
+            if student_name in i:
+                return {i[1]:('Group: '+str(i[0]),i[2],i[3],i[4])}
+    def show_all_students(self):
+        all_students = dict()
+        for i in self.group_info:
+            all_students[i[1]] = ('Group: '+str(i[0]),i[2],i[3],i[4])
+        return all_students
         
-class Group(Students,Teacher):
-    def __init__(self):
-        super().__init__()
-    
-    def rating_groups(self):
-        rating_dict = dict()
-        for i in self.students:
-            if i[0] not in rating_dict:
-                rating_dict[i[0]] = i[-1]
-            else:
-                rating_dict[i[0]] += i[-1]
-        for key in rating_dict:
-            if rating_dict.get(key)==max(rating_dict.values()):
-                best_group = key + ' Is the best gpoup'
-            elif rating_dict.get(key)==min(rating_dict.values()):
-                worst_group = key + ' Is the worst gpoup'
-            
-        return best_group+'\n'+worst_group
+        
 
-    def best_worst_student(self):
-        student_marks =[i[-1] for i in self.students]
-        for i in self.students:
-            if i[-1] == max(student_marks):
-                best_student = 'Best Studend: '+i[1]+' has '+str(i[-1])+' average mark'
-            elif i[-1] == min(student_marks):
-                worst_student = 'Worst Studend: '+i[1]+' has '+str(i[-1])+' average mark'
-        return best_student+'\n'+worst_student
+class Sort:
 
-    """            
-       We are not changing our parent class Info, but we can extend it by using arguments from it in other classes,
-       like Students, Group, other
-    """    
+    @staticmethod
+    def sort(object):
+        if type(object) == list:
+            sorted_object = sorted(object)
+            return sorted_object
+        elif type(object) == dict:
+            sorted_object = {key: value for key,value in sorted(object.items())}
+            return sorted_object
 
-name_surname_teacher = Faker()
-name_surname_student = Faker()
-a = Teacher()
-b = Students()
-c = Group()
-a.add_teachers(name_surname_teacher.name(),random.choice(a.lst_subjects))
-a.add_teachers(name_surname_teacher.name(),random.choice(a.lst_subjects))
-a.add_teachers(name_surname_teacher.name(),random.choice(a.lst_subjects))
-b.add_students(name_surname_student.name(),random.choice(a.lst_groups),random.randint(1,100))
-b.add_students(name_surname_student.name(),random.choice(a.lst_groups),random.randint(1,100))
-b.add_students(name_surname_student.name(),random.choice(a.lst_groups),random.randint(1,100))
+fake = Faker()
+a = Student()
+a.add_student(random.choice(a.groups),'Anton','20.04.1997','Dnipro','+3809999999')
+a.add_student(random.choice(a.groups),'Vasya',str(fake.date_of_birth()),fake.city(),fake.phone_number())
+a.add_student(random.choice(a.groups),'Petya',str(fake.date_of_birth()),fake.city(),fake.phone_number())
+a.add_student(random.choice(a.groups),'Masha',str(fake.date_of_birth()),fake.city(),fake.phone_number())
 
-
-for i in range(30):
-    c.add_teachers(name_surname_teacher.name(),random.choice(a.lst_subjects))
-    c.add_students(name_surname_student.name(),random.choice(a.lst_groups),random.randint(1,100))
-
-print(a.show_teachers())
-print(b.show_students())
-
-
-print(c.show_teachers())
-print(c.show_students())
-print(c.best_worst_student())
-print(c.rating_groups())
+b = Sort()
+print(b.sort(a.show_all_students()))
+print(b.sort(a.group_info))

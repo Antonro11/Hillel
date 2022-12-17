6ec4c024a750d50797c6cc1b6016cb16257fd69e
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-17 23:20:21 +0200

    Update lsp.py

--- lsp.py
+++ lsp.py
@@ -1,48 +1,46 @@
 """
-Liskov Supstitution
 
-We have same data in Students and Teachers: (name,city,phone) but we have data which is not same: (group),(salary).
-So we created one class for (name,city,phone) and two more classas for (group) and (salary)
-  
+Liskov supstitution
+
+We could make parentship between Teacher and FiredTeacher class, becouse they both can teach. 
+But it not goes to LSP, becouse they have different status.
+So we could make one Separate class Teach, or add fucntion could_teach in every class, it both will be right.
+
 
 """
 
-import random
-from faker import Faker
-    
-class PersonInfo:
-    def __init__(self,name,city,phone):
+class Person:
+    def __init__(self,name,last_name):
         self.name = name
-        self.phone = phone
-        self.city = city
-class Info_With_Group(PersonInfo):
-    def __init__(self,name,city,phone,group):
-        super().__init__(name,city,phone)
-        self.group = group
+        self.last_name = last_name
 
-class Info_with_subject(PersonInfo):
-    def __init__(self,name,city,phone,subject):
-        super().__init__(name,city,phone)
-        self.subject = subject
+class Student(Person):
+    def __init__(self, name, last_name):
+        super().__init__(name, last_name)
+        return {self.name + ' ' + self.last_name:'Student'}
 
+class Teacher(Person):
+    def __init__(self, name, last_name):
+        super().__init__(name, last_name)
+    
+    def teacher(self):
+        return {self.name + ' ' + self.last_name:'Teacher'}
+    
+    def could_teach(self):
+        return 'Could teach'
 
+class FiredTeacher(Person):
+    def __init__(self, name, last_name):
+        super().__init__(name, last_name)
     
-    
-class Student(Info_With_Group):
-    def __init__(self, name, city, phone, group):
-        super().__init__(name, city, phone, group)
-    
-    def show_student(self):
-        return {self.name:(self.city,self.phone,self.group)} 
+    def fired_teacher(self):
+        return {self.name + ' ' + self.last_name:'Fired teacher'}
 
-class Teacher(Info_with_subject):
-    def __init__(self, name, city, phone, subject):
-        super().__init__(name, city, phone, subject)
-    
-    def show_teacher(self):
-        return {self.name:(self.city,self.phone,self.subject)} 
-    
-a = Student('Anton','Dnipro','+380999999','B')
-b = Teacher('Vera','Kiev','+380777777','mathematic')
-print(a.show_student())
-print(b.show_teacher())
+    def could_teach(self):
+        return 'Could teach'
+
+a =FiredTeacher('Evgeniy', 'Batkovich')
+b = Teacher('Vasiliy','Petrovich')
+print(a.fired_teacher())
+print(b.teacher())
+

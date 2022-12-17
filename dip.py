bd7acb4b8984211c53a1adb2d2f3c88f70d6fd4c
Antonro11 <113366895+Antonro11@users.noreply.github.com>
2022-12-17 23:21:29 +0200

    Update dip.py

--- dip.py
+++ dip.py
@@ -1,61 +1,50 @@
 """
 Dependency inversion
 
-We have class Persons with some info about person and we have class Sort_student_or_teacher, wich can get
-every collection and sort it by teachers or students, and it not depends on our upper class. If we don't get data
-wich relates to students or teachers, we get massage, no sudents or teachers there.
-
 """
 
+from abc import ABC, abstractmethod
 
-
-class Persons:
-    def __init__(self):
-        self.persons = []
+class IsStudent(ABC):
     
-    def add_person(self,*args):
-        self.persons.append(args)
+    @abstractmethod
+    def go_to_unuversity(self):
+        pass
+    @abstractmethod
+    def get_marks(self):
+        pass
+
+class IsTeacher(ABC):
+
+    @abstractmethod
+    def go_to_unuversity(self):
+        pass
     
-    def get_persons(self):
-        return self.persons
+    @abstractmethod
+    def get_salary(self):
+        pass
 
+class Student(IsStudent):
 
-class Sort_student_or_teacher:
-    def __init__(self):
-        self.groups = ['A','B','C','D']
-        self.subjects = ['mathematic','culture','chemistry']
-        self.students= []
-        self.teachers = []
-        
-
-    def sort_by_persons(self,args):
-        for i in args:
-            for y in self.groups:
-                if y in i:
-                   self.students.append(i)
-        for i in args:
-            for y in self.subjects:
-                if y in i:
-                   self.teachers.append(i)
-
-    def show_students(self):
-        if len(self.students) == 0:
-            return 'No students in list'
-        return self.students
+    def go_to_unuversity(self):
+        return 'Go to university'
     
-    def show_teachers(self):
-        if len(self.teachers) == 0:
-            return 'No teachers in list'
-        return self.teachers
+    def get_marks(self):
+        return 'I can get marks'
 
+class Teacher(IsTeacher):
+
+    def go_to_unuversity(self):
+        return 'Go to university'
     
-a = Persons()
-b = Sort_student_or_teacher()
-a.add_person('Anton','B','20.04.1997')
-a.add_person('Vasya','chemistry','12,10,1991')
-b.sort_by_persons(a.get_persons())
-print(b.show_students())
-print(b.show_teachers())
+    def get_salary(self):
+        return 'I can get salary'
+
+a = Teacher()
+b =Student()
+print(a.get_salary())
+print(b.go_to_unuversity())
+                   
 
                    
                     
